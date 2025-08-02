11643950e99bb75e45eba511e3db9eaba2afdb07
erikheydari <erikheydari@gmail.com>
2025-08-02 15:16:27 +0330

    Add layout components: auth.vue, default.vue, and guest.vue with templates and basic structure

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full h-full">
+    <TheHeader />
+    <main class="w-full h-full">
+      <slot />
+    </main>
+    <TheFooter />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { TheFooter } from '~/components/footer';
+import { TheHeader } from '~/components/header';
+
+</script>
+
+<style></style>
