a72829284cc57cdddc4e4fa051d7dbfb2ccad9d3
erikheydari <erikheydari@gmail.com>
2025-08-02 15:16:09 +0330

    Add TheHeader component with template, script, and style; create index export

---
+++ app/components/header/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="w-full h-full bg-green-500">
+    <div class="w-full h-full bg-yellow-500">
+      <h1>Header</h1>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
