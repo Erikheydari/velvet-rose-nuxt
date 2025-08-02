bd2cb2a7ad23d432183b1ce48e938cc2e8ad7d41
erikheydari <erikheydari@gmail.com>
2025-08-02 15:15:27 +0330

    Add Tailwind CSS configuration, utility functions, and new components.json file; update package dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/scripts']
+  css: ['~/assets/css/tailwind.css', '~/assets/css/typography.css', '~/assets/fonts/fonts.css'],
+  modules: ['@nuxt/eslint', '@nuxt/scripts', 'shadcn-nuxt'],
+  shadcn: {
+    prefix: 'The',
+    componentDir: '~/components/ui',
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
