0a20267fe5519ff47ff288921fd6ff78085128b5
erikheydari <erikheydari@gmail.com>
2025-08-02 20:12:23 +0330

    Add useEndpointStore for centralized API endpoint management; define product, page, brand, and category endpoints based on runtime configuration.

---
+++ app/stores/endpoints.ts
@@ -0,0 +1,33 @@
+export const useEndpointStore = defineStore('endpointStore', () => {
+    const config = useRuntimeConfig();
+    const apiUrl = config.public.apiUrl;
+    const apiPrefix = config.public.apiPrefix;
+
+    const products = {
+        get: `${apiUrl}${apiPrefix}/products`,
+        detail: (id: number) => `${apiUrl}${apiPrefix}/products/${id}`,
+    }
+
+    const pages = {
+        about: `${apiUrl}${apiPrefix}/pages/about-us`,
+        contact: `${apiUrl}${apiPrefix}/pages/contact-us`,
+    }
+
+    const brands = {
+        get: `${apiUrl}${apiPrefix}/brands`,
+    }
+
+    const categories = {
+        get: `${apiUrl}${apiPrefix}/categories`,
+        products: (id: number) => `${apiUrl}${apiPrefix}/categories/${id}/products`,
+    }
+
+    return {
+        apiUrl,
+        apiPrefix,
+        products,
+        pages,
+        brands,
+        categories,
+    }
+})
