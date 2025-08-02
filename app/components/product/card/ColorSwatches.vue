ab11db0b7deed7ef35b4e5af78f9a06e4205f687
erikheydari <erikheydari@gmail.com>
2025-08-02 20:11:14 +0330

    Add ColorSwatches component with template and props for color display; implement dynamic color rendering in a flexible layout

---
+++ app/components/product/card/ColorSwatches.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center justify-end -space-x-2 hover:w-full transition-all duration-300" dir="ltr">
+    <span v-for="(color, index) in props.colors" :key="index" :style="{ backgroundColor: color }"
+      class="size-8 rounded-full border border-background hover:scale-105 transition-transform duration-300">
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  colors: string[]
+}>();
+</script>
