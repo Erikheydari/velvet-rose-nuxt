fd17814f83e55d6d34b3b4bf426316cbd6cfe3b9
erikheydari <erikheydari@gmail.com>
2025-08-02 15:16:03 +0330

    Add footer component with TheFooter.vue template and index export

---
+++ app/components/footer/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="w-full h-full bg-red-500">
+    <div class="w-full h-full bg-blue-500">
+      <h1>Footer</h1>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
