b5a0d29b244749c4e8e8f905996cec6571bb4f27
erikheydari <erikheydari@gmail.com>
2025-08-02 20:12:10 +0330

    Add product listing to index page; integrate ProductCard component and fetch products on mount for dynamic display.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,22 @@
 <template>
   <HeroSection />
+  <div class="bg-secondary py-10 px">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 default-inner-container">
+      <ProductCard v-for="product in productsStore.products" :key="product.id" :product="product" type="default" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { HeroSection } from '~/components/sections';
+import { ProductCard } from '~/components/product/card';
+import { useProductsStore } from '~/stores/products';
+
+const productsStore = useProductsStore()
+
+onMounted(async () => {
+  await productsStore.fetchProducts()
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
