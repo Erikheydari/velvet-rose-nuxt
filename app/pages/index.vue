b7298b2008ba9f6e6753b887e8678de43a363f07
erikheydari <erikheydari@gmail.com>
2025-08-02 20:41:07 +0330

    Add carousel interface: define types and props for the Carousel component, enhancing type safety and integration with embla-carousel-vue.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,11 @@
 <template>
-  <HeroSection />
-  <div class="bg-secondary py-10 px">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 default-inner-container">
-      <ProductCard v-for="product in productsStore.products" :key="product.id" :product="product" type="default" />
-    </div>
-  </div>
+  <SectionsHomeHero />
+  <SectionsHomeProducts :products="productsStore.products" type="default" title="محصولات آرایشی بهداشتی"
+    description="تمامی محصولات کاملا اورجینال و از معتبر ترین برند های دنیا میباشند" />
 </template>
 
 <script lang="ts" setup>
-import { HeroSection } from '~/components/sections';
-import { ProductCard } from '~/components/product/card';
-import { useProductsStore } from '~/stores/products';
+import { useProductsStore } from '~/stores/products'
 
 const productsStore = useProductsStore()
 
