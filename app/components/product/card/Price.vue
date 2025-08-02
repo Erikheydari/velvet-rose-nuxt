cbf7dbd33dc21544e1823ba6130fed38ad87982d
erikheydari <erikheydari@gmail.com>
2025-08-02 20:11:39 +0330

    Add Price component to display product pricing and discounts; implement template and props for final and original prices.

---
+++ app/components/product/card/Price.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex-1 flex items-baseline gap-4">
+    <bdi class="text-2xl font-semibold text-primary">
+      {{ props.product.final_price }}
+      <small>
+        تومان
+      </small>
+    </bdi>
+    <bdi v-if="props.product.discount_percentage > 0" class="text-base text-muted-foreground line-through">
+      {{ props.product.price }}
+      <small>
+        تومان
+      </small>
+    </bdi>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { Product } from '~/types/product.types';
+
+const props = defineProps<{
+  product: Product
+}>()
+</script>
+
+<style></style>
