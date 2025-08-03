ad142955192079afa28d9884386c1e6ed6510e76
erikheydari <erikheydari@gmail.com>
2025-08-03 17:33:08 +0330

    Refactor ProductCard components: adjust layout and styles for improved responsiveness and visual hierarchy, including updates to image scaling, card title, and button variants.

--- app/components/product/card/Image.vue
+++ app/components/product/card/Image.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="aspect-square w-full h-auto relative group max-w-[calc(100%-3.5rem)] mx-auto -translate-y-12">
+  <div class="aspect-square w-full h-auto relative group max-w-[calc(100%-3.5rem)] mx-auto -translate-y-[10%]">
     <span v-if="props.image && !imageError && props.type !== 'secondary'"
       :class="{ 'animate-pulse': isImageLoading, 'bg-secondary': props.type === 'default', 'bg-primary': props.type === 'primary' }"
-      class="group-hover:scale-103 transition-transform duration-300 rounded-full size-60 max-w-[90%] max-h-[90%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10"></span>
+      class="group-hover:scale-103 transition-transform duration-300 rounded-full size-[80%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10"></span>
     <figure v-if="props.image && !imageError">
       <img :src="props.image" :alt="props.alt || props.name || ''" @load="isImageLoading = false"
         @error="imageError = true"
-        class="object-contain group-hover:scale-108 group-hover:rotate-2 transition-all duration-300 origin-center"
+        class="object-contain group-hover:scale-108 group-hover:rotate-2 transition-all duration-300 origin-center h-full w-full"
         :class="{ 'opacity-0': isImageLoading || imageError, 'opacity-100': !isImageLoading && !imageError }" />
       <span
         class="w-[90%] h-4 bg-muted-foreground/70  absolute left-1/2 -translate-x-1/2 bottom-0 translate-y-3 rounded-[100%] blur-xl group-hover:opacity-100 opacity-0 transition-opacity duration-500"></span>
