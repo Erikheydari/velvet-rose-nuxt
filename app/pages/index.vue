c63c3aba4acd3dd32dcf61c7a0fa25a9490c1ef2
erikheydari <erikheydari@gmail.com>
2025-08-03 13:03:54 +0330

    Add duplicate Products component: include a second instance of SectionsHomeProducts with primary type for enhanced product display on the homepage.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,8 @@
   <SectionsHomeHero />
   <SectionsHomeProducts :products="productsStore.products" type="default" title="محصولات آرایشی بهداشتی"
     description="تمامی محصولات کاملا اورجینال و از معتبر ترین برند های دنیا میباشند" />
+    <SectionsHomeProducts :products="productsStore.products" type="primary" title="محصولات آرایشی بهداشتی"
+    description="تمامی محصولات کاملا اورجینال و از معتبر ترین برند های دنیا میباشند" />
 </template>
 
 <script lang="ts" setup>
