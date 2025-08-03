266aef5694082eb73d2a8a23ec6a19c5f3e1ee2b
erikheydari <erikheydari@gmail.com>
2025-08-03 17:33:50 +0330

    Refactor CarouselPrevious component: replace ArrowLeft icon with ChevronLeft and adjust button height for improved visual consistency and usability.

--- app/components/ui/carousel/CarouselPrevious.vue
+++ app/components/ui/carousel/CarouselPrevious.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { WithClassAsProps } from './interface'
-import { ArrowLeft } from 'lucide-vue-next'
+import { ChevronLeft } from 'lucide-vue-next'
 import { cn } from '@/lib/utils'
 import { Button, type ButtonVariants } from '@/components/ui/button'
 import { useCarousel } from './useCarousel'
@@ -22,7 +22,7 @@ const { orientation, canScrollPrev, scrollPrev } = useCarousel()
     data-slot="carousel-previous"
     :disabled="!canScrollPrev"
     :class="cn(
-      'absolute size-8 rounded-full',
+      'absolute size-8 rounded-full h-18',
       orientation === 'horizontal'
         ? 'top-1/2 -left-12 -translate-y-1/2'
         : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
@@ -33,7 +33,7 @@ const { orientation, canScrollPrev, scrollPrev } = useCarousel()
     @click="scrollPrev"
   >
     <slot>
-      <ArrowLeft />
+      <ChevronLeft />
       <span class="sr-only">Previous Slide</span>
     </slot>
   </Button>
