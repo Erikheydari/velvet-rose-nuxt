f18b030ef18844827633d99f80e8b87bd2bd5a28
erikheydari <erikheydari@gmail.com>
2025-08-03 17:33:03 +0330

    Refactor ColorSwatches component: update layout to a vertical orientation and adjust color swatch size for improved visual presentation.

--- app/components/product/card/ColorSwatches.vue
+++ app/components/product/card/ColorSwatches.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex items-center justify-end -space-x-2 hover:w-full transition-all duration-300" dir="ltr">
+  <div class="flex flex-col -space-y-2" dir="ltr">
     <span v-for="(color, index) in props.colors" :key="index" :style="{ backgroundColor: color }"
-      class="size-8 rounded-full border border-background hover:scale-105 transition-transform duration-300">
+      class="size-6 rounded-full border border-background hover:scale-105 transition-transform duration-300">
     </span>
   </div>
 </template>
