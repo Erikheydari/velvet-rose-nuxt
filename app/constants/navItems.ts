97989de748331e964c78d53c0249db78286ff2f0
erikheydari <erikheydari@gmail.com>
2025-08-04 18:26:02 +0330

    feat: items in footer

---
+++ app/constants/navItems.ts
@@ -0,0 +1,33 @@
+interface NavItem {
+    label: string;
+    to: string;
+}
+
+export const navItems: NavItem[] = [
+    { label: "صفحه اصلی", to: "/" },
+    { label: "محصولات", to: "/products" },
+    { label: "درباره ما", to: "/about-us" },
+];
+
+interface ContactItem {
+    label: string;
+    href: string;
+    target?: string;
+}
+
+export const contactItems: ContactItem[] = [
+    { label: "ایمیل: info@velvetrose.com", href: "mailto:info@velvetrose.com" },
+    { label: "شماره تماس: 1122 933 98+", href: "tel:+98112293398" },
+];
+
+interface SocialItem {
+    label: string;
+    href: string;
+    icon: string;
+}
+
+export const socialItems: SocialItem[] = [
+    { label: "اینستاگرام", href: "https://www.instagram.com/velvetrose.ir", icon: 'Instagram' },
+    { label: "توییتر", href: "https://www.twitter.com/velvetrose.ir", icon: 'X' },
+    { label: "تلگرام", href: "https://www.facebook.com/velvetrose.ir", icon: 'Telegram' },
+];
