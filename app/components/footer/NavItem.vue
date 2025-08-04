8d78846db9e2e8d4c7ffc78cd130539f415f30d9
erikheydari <erikheydari@gmail.com>
2025-08-04 18:24:51 +0330

    feat: add NavItem component to footer for improved navigation

---
+++ app/components/footer/NavItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <TheButton :to="to" :href="href" :target="target"
+    class="body-2 text-primary-foreground/70 hover:text-primary-foreground px-0 justify-start" variant="link">
+    <slot />
+  </TheButton>
+</template>
+
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  to?: string;
+  href?: string;
+  target?: string;
+  class?: string;
+}>(), {
+  to: '',
+});
+</script>
+
+<style></style>
