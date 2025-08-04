ecb2d66a43cf7ded9e47a8462dbf4570f14713aa
erikheydari <erikheydari@gmail.com>
2025-08-04 18:25:03 +0330

    feat: add Heading component for displaying titles and descriptions

---
+++ app/components/sections/Heading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="default-inner-container">
+    <h3 v-if="props.title" class="heading-3 text-primary text-center mb-4">
+      {{ props.title }}
+    </h3>
+    <p v-if="props.description" class="text-center text-muted-foreground mb-12 text-lg">
+      {{ props.description }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  title: string
+  description: string
+}>()
+
+</script>
+
+<style></style>
