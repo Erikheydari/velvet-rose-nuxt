c23f6e47c2577e39ca17d31f850d1cffeee39269
erikheydari <erikheydari@gmail.com>
2025-08-04 18:23:35 +0330

    svgo added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,18 +5,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css', '~/assets/css/typography.css', '~/assets/fonts/fonts.css'],
-  modules: ['@nuxt/eslint', '@nuxt/scripts', 'shadcn-nuxt', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint', '@nuxt/scripts', 'shadcn-nuxt', '@pinia/nuxt', 'nuxt-svgo'],
   shadcn: {
     prefix: 'The',
     componentDir: '~/components/ui',
   },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   runtimeConfig: {
     public: {
       apiUrl: process.env.API_URL || 'http://78.157.42.110',
-      apiPrefix: process.env.API_PREFIX || '/api'
+      apiPrefix: process.env.API_PREFIX || '/api',
     },
   },
-})
+});
