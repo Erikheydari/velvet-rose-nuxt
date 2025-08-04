f549c55ed9c7073d55208107371a329ba7f6fc8f
erikheydari <erikheydari@gmail.com>
2025-08-04 20:15:45 +0330

    feat: enhance header component with sticky behavior and navigation buttons

--- app/components/header/TheHeader.vue
+++ app/components/header/TheHeader.vue
@@ -1,13 +1,51 @@
 <template>
-  <header class="w-full h-full bg-green-500">
-    <div class="w-full h-full bg-yellow-500">
-      <h1>Header</h1>
+  <header class="w-full sticky top-0 z-50 transition-all duration-300 -mb-24"
+    :class="{ 'bg-background-glass shadow-sm': isSticky }">
+    <div class="w-full py-4 px-8 transition-all duration-300" :class="{ 'py-2': isSticky }">
+      <div class="w-full flex items-center justify-between">
+        <div class="flex items-center gap-2">
+          <TheButton variant="tonal" size="lg">
+            <User />
+            <span>ورود / ثبت نام</span>
+          </TheButton>
+          <TheButton variant="tonal" size="icon-lg">
+            <Search />
+          </TheButton>
+        </div>
+
+        <div class="flex items-center gap-2">
+          <ul class="flex items-center gap-2">
+            <li v-for="item in headerNavItems" :key="item.label">
+              <TheButton variant="link" class="text-muted-foreground hover:text-primary" size="lg"
+                :class="{ 'text-primary': $route.path === item.to }" :to="item.to">
+                {{ item.label }}
+              </TheButton>
+            </li>
+          </ul>
+        </div>
+
+        <Logo class="transition-all duration-300 flex-shrink-0" :class="{ 'h-12': isSticky, 'h-16': !isSticky }" />
+      </div>
     </div>
   </header>
 </template>
 
 <script lang="ts" setup>
+import Logo from '~/assets/icons/logo.svg?component'
+import { User, Search } from 'lucide-vue-next';
+import { useWindowScroll } from '@vueuse/core';
+import { headerNavItems, headerUserItems } from '~/constants/headerNavItems';
 
+const { y } = useWindowScroll();
+
+
+// Add some debouncing to prevent excessive updates
+const isSticky = computed(() => y.value > 100);
 </script>
 
-<style></style>
+<style>
+.bg-background-glass {
+  backdrop-filter: blur(20px);
+  background-color: rgba(var(--background), 0.95);
+}
+</style>
