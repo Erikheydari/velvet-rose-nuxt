b9967c792170701e577c4584451870fac5a9d898
erikheydari <erikheydari@gmail.com>
2025-08-06 14:49:33 +0330

    feat: create mobile menu list component with category loading and dynamic menu rendering

---
+++ app/components/menu/mobile/list/index.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref, computed, onMounted } from 'vue'
+import { useCategoriesStore } from '~/stores/categories'
+import type { Category } from '~/types/product.types'
+// import ProductCategoriesList from './CategoriesList.vue'
+import { headerMainMenu, headerContact, headerSocials } from '~/constants/headerNavItems'
+
+// Types definitions
+interface Props {
+  activeTab: string
+}
+
+const props = defineProps<Props>()
+const route = useRoute()
+
+// State
+const categoriesStore = useCategoriesStore()
+
+const mainMenu = computed(() => {
+  return {
+    mainMenu: headerMainMenu,
+    contact: headerContact,
+    socials: headerSocials
+  }
+})
+
+const loadCategories = async () => {
+  try {
+    const response = await categoriesStore.fetchCategories()
+    return response
+  } catch (error) {
+    console.error('Failed to load categories:', error)
+  }
+}
+
+// Lifecycle hooks
+onMounted(async () => {
+  await Promise.all([
+    loadCategories(),
+  ])
+})
+</script>
+
+<template>
+  <div class="flex flex-col h-full">
+    <!-- Product Categories Tab -->
+    <!-- <ProductCategoriesList v-if="activeTab === 'productCategories'" :main-categories="mainCategories" /> -->
+
+    <!-- Website Pages Tab -->
+    <MenuMobileListPages v-if="activeTab === 'websitePages'" :main-menu-data="mainMenu" />
+  </div>
+</template>
+
+<style scoped>
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.3s ease;
+  max-height: 1000px;
+  overflow: hidden;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  max-height: 0;
+  overflow: hidden;
+}
+</style>
