60c1d30fc988f05fbfac4ff2c501010fffae98bd
erikheydari <erikheydari@gmail.com>
2025-08-06 16:39:27 +0330

    refactor: enhance Button component to support 'href' prop for link functionality alongside 'to' prop

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -3,30 +3,26 @@ import type { HTMLAttributes } from 'vue'
 import { Primitive, type PrimitiveProps } from 'reka-ui'
 import { cn } from '@/lib/utils'
 import { type ButtonVariants, buttonVariants } from '.'
-import {NuxtLink} from "#components";
+import { NuxtLink } from "#components";
 
 interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   class?: HTMLAttributes['class']
   to?: string
+  href?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
   as: 'button',
 })
 
-const componentTag = computed(() => props.to ? NuxtLink : props.as)
+const componentTag = computed(() => (props.to ? NuxtLink : props.href ? 'a' : props.as))
 </script>
 
 <template>
-  <Primitive
-    :data-slot="props.to ? 'link' : 'button'"
-    :as="componentTag"
-    :as-child="asChild"
-    :to="props.to"
-    :class="cn(buttonVariants({ variant, size }), props.class)"
-  >
+  <Primitive :data-slot="props.to || props.href ? 'link' : 'button'" :as="componentTag" :as-child="asChild"
+    :href="props.href" :to="props.to" :class="cn(buttonVariants({ variant, size }), props.class)">
     <slot />
   </Primitive>
 </template>
