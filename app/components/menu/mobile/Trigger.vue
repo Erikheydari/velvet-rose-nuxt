6876f58d7cd4c9a3b95b7d5de0551555ecb06ffd
erikheydari <erikheydari@gmail.com>
2025-08-06 14:01:33 +0330

    feat: add mobile menu trigger component for toggling menu visibility

---
+++ app/components/menu/mobile/Trigger.vue
@@ -0,0 +1,17 @@
+<template>
+  <TheButton variant="tonal" size="icon-lg" class="shrink-0 md:hidden" @click="toggleMenu">
+    <Menu class="w-4 h-4" />
+  </TheButton>
+</template>
+
+<script lang="ts" setup>
+import { Menu } from 'lucide-vue-next'
+
+const emit = defineEmits(['toggle'])
+
+const toggleMenu = () => {
+  emit('toggle')
+}
+</script> 
+
+<style></style>
