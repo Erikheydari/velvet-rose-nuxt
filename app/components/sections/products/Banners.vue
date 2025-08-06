72052531dc2ae55deec3f4619efe0897d1688200
erikheydari <erikheydari@gmail.com>
2025-08-06 19:38:38 +0330

    feat: add Banners component for displaying product promotional images in a responsive layout

---
+++ app/components/sections/products/Banners.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col default-inner-container lg:flex-row gap-4 default-padding-x mb-8 lg:mb-12">
+    <div class="image-container">
+      <img src="/images/products/banner-1.webp" alt="banner" />
+    </div>
+    <div class="image-container">
+      <img src="/images/products/banner-2.webp" alt="banner" />
+    </div>
+    <div class="image-container">
+      <img src="/images/products/banner-3.webp" alt="banner" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style></style>
