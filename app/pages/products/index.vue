356512c7206e6b1f9529c8e414ad718b0d2f7431
erikheydari <erikheydari@gmail.com>
2025-08-06 19:38:48 +0330

    feat: integrate product grid and banners components, fetch products on mount for enhanced product display

--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
     <SectionsProductsHero />
+    <SectionsProductsBanners />
+    <ProductGrid>
+      <ProductCard v-for="product in productsStore.products" :key="product.id" :product="product" type="default" />
+    </ProductGrid>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useProductsStore } from '@/stores/products';
+import { ProductCard } from '@/components/product/card';
+
+const productsStore = useProductsStore();
+
+onMounted(() => {
+  productsStore.fetchProducts();
+});
 
 </script>
 
