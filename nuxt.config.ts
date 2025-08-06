ddae293936c726782c0519565160b408d95b5f32
erikheydari <erikheydari@gmail.com>
2025-08-06 16:37:56 +0330

    feat: add transitions CSS file to enhance styling capabilities

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/tailwind.css', '~/assets/css/typography.css', '~/assets/fonts/fonts.css'],
+  css: ['~/assets/css/tailwind.css', '~/assets/css/typography.css', '~/assets/fonts/fonts.css', '~/assets/css/transitions.css'],
   modules: ['@nuxt/eslint', '@nuxt/scripts', 'shadcn-nuxt', '@pinia/nuxt', 'nuxt-svgo'],
   shadcn: {
     prefix: 'The',
