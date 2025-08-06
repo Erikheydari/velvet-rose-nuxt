d4b7fcf37c71801d0b7618a6307f32e1560cd873
erikheydari <erikheydari@gmail.com>
2025-08-06 12:16:47 +0330

    feat: add Backdrop component with transition effects and click handling

---
+++ app/components/ui/backdrop/Backdrop.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Backdrop -->
+  <Teleport to="body">
+    <Transition enter-active-class="transition-opacity duration-200" enter-from-class="opacity-0"
+      enter-to-class="opacity-100" leave-active-class="transition-opacity duration-200" leave-from-class="opacity-100"
+      leave-to-class="opacity-0">
+      <div v-if="isActive" class="fixed inset-0 bg-background/50 backdrop-blur-sm z-40" @click="handleClose" />
+    </Transition>
+  </Teleport>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  isActive: boolean
+  handleClose: () => void
+}>()
+
+</script>
+
+<style></style>
