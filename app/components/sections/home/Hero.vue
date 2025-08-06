0b97342f1d966a8d7f42a18b5f2c7cda34c78bf4
erikheydari <erikheydari@gmail.com>
2025-08-06 14:50:21 +0330

    style: update hero section height for responsive design and clean up video source tag

--- app/components/sections/home/Hero.vue
+++ app/components/sections/home/Hero.vue
@@ -1,10 +1,11 @@
 <template>
-  <section class="w-full h-screen flex items-center justify-center bg-video relative bg-background -mb-20 lg:mb-0">
-    <h1 class="heading-1 text-primary font-italiana absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-nowrap">
+  <section class="w-full hero-height flex items-center justify-center bg-video relative bg-background -mb-20 lg:mb-0">
+    <h1
+      class="heading-1 text-primary font-italiana absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-nowrap">
       Velvet Rose
     </h1>
     <video autoplay muted loop class="w-full h-full absolute top-0 left-0 object-cover ">
-      <source src="/videos/hero.mp4" type="video/mp4"/>
+      <source src="/videos/hero.mp4" type="video/mp4" />
     </video>
     <div class="absolute left-0 w-full h-[50vh] bottom-0 z-10 bg-gradient-to-t from-background to-transparent"></div>
   </section>
@@ -20,5 +21,15 @@
   background-size: cover;
   background-position: center;
   background-repeat: repeat;
+}
+
+.hero-height {
+  height: calc(100vh + var(--spacing) * 36);
+}
+
+@media (min-width: 1024px) {
+  .hero-height {
+    height: 100vh;
+  }
 }
 </style>
