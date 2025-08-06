980c9ec3b1a27ab2cb64b9fc7f9ef35a87c38131
erikheydari <erikheydari@gmail.com>
2025-08-06 19:38:33 +0330

    feat: add product grid component for displaying products in a responsive layout

---
+++ app/components/product/grid/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="default-inner-container default-padding-x">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
