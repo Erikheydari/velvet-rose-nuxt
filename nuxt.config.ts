014c1fc500cf8a9f4b683aaeb5073d33188ce7f7
erikheydari <erikheydari@gmail.com>
2025-08-09 20:45:29 +0330

    disable devtools in nuxt configuration and add app settings including baseURL and head title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,29 @@ import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['~/assets/css/tailwind.css', '~/assets/css/typography.css', '~/assets/fonts/fonts.css', '~/assets/css/transitions.css'],
   modules: ['@nuxt/eslint', '@nuxt/scripts', 'shadcn-nuxt', '@pinia/nuxt', 'nuxt-svgo'],
   shadcn: {
     prefix: 'The',
     componentDir: '~/components/ui',
   },
+
+  app: {
+    baseURL: '/',
+/*     pageTransition: {
+      name: 'page',
+      mode: 'out-in',
+      duration: 900,
+    }, */
+    head: {
+      title: 'Velvet Rose',
+/*       link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ] */
+    }
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
