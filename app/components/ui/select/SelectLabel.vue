d19d5f99f9a0ecd818e0b8e5e2a3855e1726d6b7
erikheydari <erikheydari@gmail.com>
2025-08-09 17:07:33 +0330

    add Select component and related subcomponents for enhanced UI selection functionality

---
+++ app/components/ui/select/SelectLabel.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { SelectLabelProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { SelectLabel } from "reka-ui"
+import { cn } from "@/lib/utils"
+
+const props = defineProps<SelectLabelProps & { class?: HTMLAttributes["class"] }>()
+</script>
+
+<template>
+  <SelectLabel
+    data-slot="select-label"
+    :class="cn('px-2 pt-1.5 pb-3 mb-2 text-sm font-medium text-end border-b border-b-border', props.class)"
+  >
+    <slot />
+  </SelectLabel>
+</template>
