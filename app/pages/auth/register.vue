3fe40a8cbde51bfe087c2bacb45af74f11f3d3dd
erikheydari <erikheydari@gmail.com>
2025-08-09 20:28:29 +0330

    refactor registration page layout and meta information for improved readability

--- app/pages/auth/register.vue
+++ app/pages/auth/register.vue
@@ -2,28 +2,25 @@
 import { useHead } from '#imports'
 
 definePageMeta({
-  layout: 'auth',
-  middleware: 'guest'
+    layout: 'auth',
+    middleware: 'guest'
 })
 
 useHead({
-  title: 'ثبت نام',
-  meta: [
-    { name: 'description', content: 'ثبت نام' }
-  ]
+    title: 'ثبت نام',
+    meta: [
+        { name: 'description', content: 'ثبت نام' }
+    ]
 })
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 w-full">
-    <h1 class="heading-5 font-bold text-primary text-center w-full">
-      ثبت نام
-    </h1>
-    <FormRegister />
-    <TheButton variant="ghost" size="lg" class="w-full absolute bottom-0 right-0 rounded-none" to="/auth/login">
-      ورود به حساب
-    </TheButton>
-  </div>
+    <div class="flex flex-col gap-4 w-full">
+        <h1 class="heading-5 font-bold text-primary text-center w-full">
+            ثبت نام
+        </h1>
+        <FormRegister />
+    </div>
 </template>
 
-<style scoped></style> 
+<style scoped></style>
