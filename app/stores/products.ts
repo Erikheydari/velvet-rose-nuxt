1bb95a7829428400b473866b1300d9c4b08b0bc2
erikheydari <erikheydari@gmail.com>
2025-08-09 13:05:01 +0330

    get single product fixed

--- app/stores/products.ts
+++ app/stores/products.ts
@@ -8,6 +8,7 @@ export const useProductsStore = defineStore('productsStore', () => {
   const apiStore = useApiStore()
 
   const products = ref<Product[]>([])
+  const product = ref<Product | null>(null)
 
   const fetchProducts = async () => {
     const { data, error } = await apiStore.apiRequest(endpointStore.products.get, {
@@ -18,10 +19,19 @@ export const useProductsStore = defineStore('productsStore', () => {
     }
   }
 
-
+  const fetchProduct = async (identifier: string | number) => {
+    const { data, error } = await apiStore.apiRequest(endpointStore.products.detail(identifier), {
+      method: 'get',
+    });
+    if (data) {
+      product.value = data as Product
+    }
+  }
   return {
     products,
+    product,
 
     fetchProducts,
+    fetchProduct,
   }
 })
