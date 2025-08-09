7a85e807db0e501c94b35b205f73a587571f6783
erikheydari <erikheydari@gmail.com>
2025-08-09 15:06:34 +0330

    update ColorSwatches component to use structured color objects

--- app/components/product/card/ColorSwatches.vue
+++ app/components/product/card/ColorSwatches.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col -space-y-2" dir="ltr">
-    <span v-for="(color, index) in props.colors" :key="index" :style="{ backgroundColor: color }"
+    <span v-for="(color, index) in props.colors" :key="index" :style="{ backgroundColor: color.value }"
       class="size-6 rounded-full border border-background hover:scale-105 transition-transform duration-300">
     </span>
   </div>
@@ -8,6 +8,9 @@
 
 <script lang="ts" setup>
 const props = defineProps<{
-  colors: string[]
+  colors: {
+    value: string
+    name: string
+  }[]
 }>();
 </script>
