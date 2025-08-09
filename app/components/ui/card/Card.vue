17545eb977c34644390e66a1e92962e061c4892f
erikheydari <erikheydari@gmail.com>
2025-08-09 13:03:48 +0330

    card get to attribute with nuxtlink now

--- app/components/ui/card/Card.vue
+++ app/components/ui/card/Card.vue
@@ -1,22 +1,27 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
+import { NuxtLink } from '#components'
 
 const props = defineProps<{
   class?: HTMLAttributes['class']
+  to?: string
 }>()
+
+const componentIs = computed(() => {
+  if (props.to) {
+    return NuxtLink
+  }
+  return 'div'
+})
 </script>
 
 <template>
-  <div
-    data-slot="card"
-    :class="
-      cn(
-        'bg-card text-card-foreground flex flex-col gap-4 rounded-4xl py-6',
-        props.class,
-      )
-    "
-  >
+  <component :is="componentIs" :to="props.to" data-slot="card" :class="cn(
+    'bg-card text-card-foreground flex flex-col gap-4 rounded-4xl py-6',
+    props.class,
+  )
+    ">
     <slot />
-  </div>
+  </component>
 </template>
