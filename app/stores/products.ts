cb0aa6d959b299d6b5b128c79b14c4dfa2fb85b9
erikheydari <erikheydari@gmail.com>
2025-08-09 17:08:12 +0330

    enhance product fetching functionality with loading state management and add method to fetch products by category

--- app/stores/products.ts
+++ app/stores/products.ts
@@ -7,31 +7,55 @@ export const useProductsStore = defineStore('productsStore', () => {
   const endpointStore = useEndpointStore()
   const apiStore = useApiStore()
 
+  const isLoading = ref(true)
   const products = ref<Product[]>([])
   const product = ref<Product | null>(null)
 
   const fetchProducts = async () => {
+    isLoading.value = true
     const { data, error } = await apiStore.apiRequest(endpointStore.products.get, {
       method: 'get',
     });
     if (data) {
       products.value = data as Product[]
     }
+    isLoading.value = false
   }
 
   const fetchProduct = async (identifier: string | number) => {
-    const { data, error } = await apiStore.apiRequest(endpointStore.products.detail(identifier), {
+    isLoading.value = true
+    product.value = null
+    try {
+      const { data } = await apiStore.apiRequest(endpointStore.products.detail(identifier), {
+        method: 'get',
+      });
+      if (data) {
+        product.value = data as Product
+      }
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  const fetchProductsByCategory = async (categorySlug: string) => {
+    isLoading.value = true
+    const { data, error } = await apiStore.apiRequest(endpointStore.products.getByCategory(categorySlug), {
       method: 'get',
     });
     if (data) {
-      product.value = data as Product
+      products.value = data as Product[]
     }
+    isLoading.value = false
   }
+
   return {
+    isLoading,
+
     products,
     product,
 
     fetchProducts,
     fetchProduct,
+    fetchProductsByCategory,
   }
 })
