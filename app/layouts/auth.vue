b9a3df40c21f649cbe99ae08288293c213b175be
erikheydari <erikheydari@gmail.com>
2025-08-09 20:28:14 +0330

    add navigation buttons to auth layout for seamless transition between login, registration, and password recovery

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { ChevronRight } from 'lucide-vue-next'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
 </script>
 
 <template>
@@ -13,6 +16,21 @@ import { ChevronRight } from 'lucide-vue-next'
       class="default-inner-container w-full flex flex-row h-2/3 justify-between border-border border rounded-2xl overflow-hidden ">
       <div class="flex flex-col gap-4 justify-center items-center grow xl:p-12 relative">
         <slot />
+        <div class="w-full flex gap-4 flex-row justify-between">
+          <TheButton v-if="route.path === '/auth/register'" variant="ghost" class="grow" to="/auth/login">
+            ورود به حساب
+          </TheButton>
+          <TheButton v-if="route.path === '/auth/login'" variant="ghost" to="/auth/register">
+            ثبت نام
+          </TheButton>
+          <TheButton v-if="route.path === '/auth/login'" variant="ghost" to="/auth/forgot-password">
+            فراموشی رمز عبور
+          </TheButton>
+          <TheButton v-if="route.path === '/auth/forgot-password'" variant="ghost" class="grow" to="/auth/login">
+            بازگشت به ورود
+          </TheButton>
+        </div>
+
       </div>
       <div class="basis-2/3 overflow-hidden relative">
         <video src="/videos/hero.mp4" autoplay muted loop
