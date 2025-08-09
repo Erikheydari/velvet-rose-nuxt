96baf28ee4082ce742e6950f04f267bef071eb41
erikheydari <erikheydari@gmail.com>
2025-08-09 15:06:50 +0330

    enhance grid layout by increasing gap size for better spacing

--- app/components/product/grid/index.vue
+++ app/components/product/grid/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="default-inner-container default-padding-x">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
       <slot />
     </div>
   </section>
