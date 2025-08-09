9a90c9e915ea64b5ff7455eb2b29cb63bbfa8c2f
erikheydari <erikheydari@gmail.com>
2025-08-09 20:07:25 +0330

    enhance Input component to support dynamic input types by adding a 'type' prop with a default value of 'text'

--- app/components/ui/input/Input.vue
+++ app/components/ui/input/Input.vue
@@ -2,12 +2,16 @@
 import type { HTMLAttributes } from "vue"
 import { useVModel } from "@vueuse/core"
 import { cn } from "@/lib/utils"
+import type { InputTypeHTMLAttribute } from "vue"
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   defaultValue?: string | number
   modelValue?: string | number
   class?: HTMLAttributes["class"]
-}>()
+  type?: InputTypeHTMLAttribute
+}>(), {
+  type: 'text'
+})
 
 const emits = defineEmits<{
   (e: "update:modelValue", payload: string | number): void
@@ -23,6 +27,7 @@ const modelValue = useVModel(props, "modelValue", emits, {
   <input
     v-model="modelValue"
     data-slot="input"
+    :type="type"
     :class="cn(
       'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-primary flex h-9 w-full min-w-0 rounded-full border bg-input px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
       'focus-visible:border-ring focus-visible:ring-ring/20 focus-visible:ring-4 focus-visible:placeholder:text-primary/80',
