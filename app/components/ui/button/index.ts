491bb1ded7619a56ac5d88c973daeb0bdfba0af5
erikheydari <erikheydari@gmail.com>
2025-08-09 20:28:05 +0330

    update button variant styles to change hover text color for ghost variant

--- app/components/ui/button/index.ts
+++ app/components/ui/button/index.ts
@@ -16,7 +16,7 @@ export const buttonVariants = cva(
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost:
-          'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
+          'hover:bg-accent hover:text-primary dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
         tonal: 'bg-secondary/40 text-primary hover:bg-secondary/20',
       },
