024c7f6404792ad5aec05a315f10e335df40d2e3
erikheydari <erikheydari@gmail.com>
2025-08-10 19:17:00 +0330

    Add Product skeleton component for loading state representation, enhancing user experience during data fetch.

---
+++ app/components/skeleton/Product.vue
@@ -0,0 +1,10 @@
+<template>
+  <Skeleton class="aspect-[328/455] rounded-xl" />
+</template>
+
+<script lang="ts" setup>
+import { Skeleton } from '~/components/ui/skeleton'
+
+</script>
+
+<style></style>
