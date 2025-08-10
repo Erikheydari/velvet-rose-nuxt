bb9c8e362069f496d2ddd5869306fa3a36d100d7
erikheydari <erikheydari@gmail.com>
2025-08-10 19:14:56 +0330

    Refactor SearchCard component layout and styling for improved responsiveness, adjusting padding and image positioning for better visual presentation.

--- app/components/product/search-card/index.vue
+++ app/components/product/search-card/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex gap-4 w-full rounded-2xl border border-border p-4">
-    <figure v-if="props.product.image" class="image-container w-58 h-auto bg-muted">
-      <img :src="props.product.image.src" alt="product" class="aspect-square object-cover" />
+  <div class="flex gap-4 w-full rounded-2xl border border-border p-2 lg:p-4">
+    <figure v-if="props.product.image" class="image-container aspect-square w-1/4 lg:max-w-58 h-auto bg-muted relative">
+      <img :src="props.product.image.src" alt="product" class="object-contain absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full" />
     </figure>
-    <div class="content flex flex-col gap-2 items-start py-6 justify-between">
+    <div class="content flex flex-col gap-2 items-start py-2 lg:py-6 justify-between">
       <div>
         <h3 class="heading-5 mb-2 font-bold">{{ props.product.name }}</h3>
         <div class="flex gap-2 flex-wrap">
@@ -21,7 +21,7 @@
 
 <script lang="ts" setup>
 import type { Product } from '~/types/product.types';
-import { ProductCardPrice } from '../card';
+import ProductCardPrice from '@/components/product/card/Price.vue';
 import ColorSwatches from '@/components/product/card/ColorSwatches.vue';
 
 const props = defineProps<{
