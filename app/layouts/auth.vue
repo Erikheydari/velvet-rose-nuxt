dea0500a3b6f8f232c441cf931e2d7154dd8929b
erikheydari <erikheydari@gmail.com>
2025-08-10 19:17:24 +0330

    Refactor auth layout to enhance responsiveness and visual hierarchy, including adjustments to flex properties and z-index for improved display.

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -33,15 +33,15 @@ onMounted(() => {
 </script>
 
 <template>
-  <section class="flex flex-row w-full h-screen justify-center items-center">
+  <section class="flex w-full h-screen justify-center items-center p-8 lg:p-0">
     <TheButton variant="link" to="/" class="px-0! absolute top-0 right-0">
       <ChevronRight class="size-4" />
       بازگشت
     </TheButton>
 
     <div
-      class="default-inner-container w-full flex flex-row h-2/3 justify-between border-border border rounded-2xl overflow-hidden ">
-      <div class="flex flex-col gap-4 justify-center items-center grow xl:p-12 relative">
+      class="default-inner-container w-full flex flex-col-reverse lg:flex-row lg:h-2/3 justify-between border-border border rounded-2xl overflow-hidden ">
+      <div class="flex flex-col gap-4 justify-center items-center grow px-6 py-8 xl:px-12 xl:py-12 relative z-10 background-backdrop">
         <slot />
         <div class="w-full flex gap-4 flex-row justify-between">
           <TheButton v-if="route.path === '/auth/register'" variant="ghost" class="grow" to="/auth/login">
@@ -59,7 +59,10 @@ onMounted(() => {
         </div>
 
       </div>
-      <div class="basis-2/3 overflow-hidden relative">
+      <div class="lg:basis-2/3 overflow-hidden fixed lg:relative w-full h-full inset-0 z-0">
+        <h1 class="heading-2 text-nowrap font-italiana w-full flex justify-center items-center py-8 relative lg:absolute lg:top-1/2 lg:left-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2 z-11 text-primary">
+          Velvet Rose
+        </h1>
         <video src="/videos/hero.mp4" autoplay muted loop
           class="w-full object-cover h-full absolute top-0 left-0 z-10"></video>
       </div>
