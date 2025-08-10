2770bbd3f8db9000d58d2b3e4b2e7d44abcacb4e
erikheydari <erikheydari@gmail.com>
2025-08-10 19:14:30 +0330

    Remove ProductCard component and its related exports from the product card module.

--- app/components/product/card/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProductCard } from './index.vue'
-export { default as ProductCardImage } from './Image.vue'
-export { default as ProductCardColorSwatches } from './ColorSwatches.vue'
-export { default as ProductCardPrice } from './Price.vue'
