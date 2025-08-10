e20171b6c82b64900664c511fc0fdf240aed2f13
erikheydari <erikheydari@gmail.com>
2025-08-10 19:17:09 +0330

    Update z-index of backdrop component to improve stacking context and ensure proper display order.

--- app/components/ui/backdrop/Backdrop.vue
+++ app/components/ui/backdrop/Backdrop.vue
@@ -4,7 +4,7 @@
     <Transition enter-active-class="transition-opacity duration-200" enter-from-class="opacity-0"
       enter-to-class="opacity-100" leave-active-class="transition-opacity duration-200" leave-from-class="opacity-100"
       leave-to-class="opacity-0">
-      <div v-if="isActive" class="fixed inset-0 background-backdrop z-50" @click="handleClose" />
+      <div v-if="isActive" class="fixed inset-0 background-backdrop z-49" @click="handleClose" />
     </Transition>
   </Teleport>
 </template>
