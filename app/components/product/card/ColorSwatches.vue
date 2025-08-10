ea51a3b598359e738014cfe97d2795a506f6df71
erikheydari <erikheydari@gmail.com>
2025-08-10 15:31:13 +0330

    Update ColorSwatches component to support orientation prop for flexible layout options.

--- app/components/product/card/ColorSwatches.vue
+++ app/components/product/card/ColorSwatches.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col -space-y-2" dir="ltr">
+  <div class="flex -space-y-2" :class="props.orientation === 'horizontal' ? 'flex-row' : 'flex-col'" dir="ltr">
     <span v-for="(color, index) in props.colors" :key="index" :style="{ backgroundColor: color.value }"
       class="size-6 rounded-full border border-background hover:scale-105 transition-transform duration-300">
     </span>
@@ -8,6 +8,7 @@
 
 <script lang="ts" setup>
 const props = defineProps<{
+  orientation?: 'horizontal' | 'vertical'
   colors: {
     value: string
     name: string
