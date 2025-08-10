4810014d4ddbdeee4860d217987dca33809b01a3
erikheydari <erikheydari@gmail.com>
2025-08-10 19:14:22 +0330

    Enhance ColorSwatches component by adding size prop support, allowing dynamic sizing of color swatches based on user preference, and updating class bindings for improved styling.

--- app/components/product/card/ColorSwatches.vue
+++ app/components/product/card/ColorSwatches.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="flex -space-y-2" :class="props.orientation === 'horizontal' ? 'flex-row' : 'flex-col'" dir="ltr">
     <span v-for="(color, index) in props.colors" :key="index" :style="{ backgroundColor: color.value }"
-      class="size-6 rounded-full border border-background hover:scale-105 transition-transform duration-300">
+      class="rounded-full border-2 border-background hover:scale-105 transition-transform duration-300"
+      :class="sizeClass">
     </span>
   </div>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps<{
+const props = withDefaults(defineProps<{
+  size?: 'sm' | 'md' | 'lg'
   orientation?: 'horizontal' | 'vertical'
   colors: {
     value: string
     name: string
   }[]
-}>();
+}>(), {
+  size: 'md'
+});
+
+const sizeClass = computed(() => {
+  switch (props.size) {
+    case 'sm':
+      return 'size-4 lg:size-5'
+    case 'md':
+      return 'size-5 lg:size-6'
+    case 'lg':
+      return 'size-6 lg:size-7'
+  }
+})
 </script>
