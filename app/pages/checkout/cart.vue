720d6442e3168fb36c339feeb40a13dc595ee2ed
erikheydari <erikheydari@gmail.com>
2025-08-11 19:06:03 +0330

    Refactor cart component to improve loading state handling and display logic, including a new condition for rendering the empty cart message and enhanced error handling during cart fetch.

--- app/pages/checkout/cart.vue
+++ app/pages/checkout/cart.vue
@@ -4,19 +4,24 @@
     <div class="flex flex-col lg:flex-row gap-4 lg:gap-8 lg:p-8 p-4 border-border border rounded-2xl bg-accent/20">
 
       <div class="flex flex-col gap-4 grow">
-        <template v-if="!cartStore.loading">
+        <template v-if="!isStoreReady || cartStore.loading">
+          <TheSkeleton v-for="i in 3" :key="i" class="w-full h-32" />
+        </template>
+
+
+        <template v-else-if="isStoreReady && !cartStore.loading && cartStore.cartItems.length > 0">
           <ProductCartCard class="bg-background" v-for="item in cartStore.cartItems" :key="item.id"
             :product="item.product" :item-id="item.id" :quantity="item.quantity" />
         </template>
 
-        <template v-if="cartStore.loading">
-          <TheSkeleton v-for="i in 3" :key="i" class="w-full h-32" />
+
+        <template v-else-if="isStoreReady && cartStore.cartItems.length === 0 && !cartStore.loading">
+          <CartEmpty class="h-full flex flex-col justify-center items-center gap-4" />
         </template>
 
       </div>
 
-      <CartBox :button-text="'تکمیل سفارش'"
-      :loading="cartStore.loading" :cart-items="cartStore.cartItems"/>
+      <CartBox v-if="isStoreReady" :button-text="'تکمیل سفارش'" :loading="cartStore.loading" :cart-items="cartStore.cartItems" />
 
     </div>
   </div>
@@ -24,14 +29,18 @@
 
 <script lang="ts" setup>
 import { useCartStore } from '@/stores/cart';
-import { usePaymentsStore } from '@/stores/payments';
 import { ProductCartCard } from '#components';
+import { onMounted, computed } from 'vue';
 
 const cartStore = useCartStore();
-const paymentStore = usePaymentsStore();
+const isStoreReady = computed(() => !!cartStore);
 
 onMounted(async () => {
-  await cartStore.fetchCart();
+  try {
+    await cartStore.fetchCart();
+  } catch (error) {
+    console.error('Failed to fetch cart:', error);
+  }
 })
 </script>
 
