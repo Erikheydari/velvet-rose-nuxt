a0afb38459ec93ac2e4ccd41520b776503360e6c
erikheydari <erikheydari@gmail.com>
2025-08-11 19:06:29 +0330

    Add Pinia store import in endpoints.ts to enhance state management capabilities.

--- app/stores/endpoints.ts
+++ app/stores/endpoints.ts
@@ -1,3 +1,5 @@
+import { defineStore } from 'pinia';
+
 export const useEndpointStore = defineStore('endpointStore', () => {
     const config = useRuntimeConfig();
     const apiUrl = config.public.apiUrl;
