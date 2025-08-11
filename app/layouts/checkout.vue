cfcf332635931a33d8f743e794c1d29d004fa194
erikheydari <erikheydari@gmail.com>
2025-08-11 16:38:54 +0330

    Add checkout layout component with sections for cart, shipping information, and payment, enhancing the checkout process structure.

---
+++ app/layouts/checkout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-full h-full flex flex-col">
+    <div class="w-full">
+      <ul class="flex flex-row gap-4 lg:gap-8 justify-between">
+        <li>
+          سبد خرید
+        </li>
+        <li>
+          اطلاعات ارسال
+        </li>
+        <li>
+          پرداخت
+        </li>
+      </ul>
+    </div>
+    <div class="w-full">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+
+</script>
+
+<style></style>
