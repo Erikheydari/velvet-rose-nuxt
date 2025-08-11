ba26a5f69f802de54e5bbaadb1a1493c4b6ffd5e
erikheydari <erikheydari@gmail.com>
2025-08-11 18:39:11 +0330

    Enhance payment page layout by integrating CheckoutLayout and CartBox components, improving payment initiation flow and redirecting to cart if empty. Update page title dynamically based on payment authority.

--- app/pages/checkout/payment.vue
+++ app/pages/checkout/payment.vue
@@ -1,17 +1,44 @@
 <template>
-  <div>
-    
-  </div>
+  <CheckoutLayout>
+    <FormSetOrder />
+    <template #cart-box>
+      <CartBox :loading="cartStore.loading" :disabled="!paymentStore.paymentAuthority"
+        :handle-click="() => paymentStore.initiatePayment()" :cart-items="cartStore.cartItems" button-text="پرداخت" />
+    </template>
+  </CheckoutLayout>
 </template>
 
 <script lang="ts" setup>
-  
-definePageMeta({
-  layout: 'checkout',
+import CheckoutLayout from '@/layouts/checkout.vue';
+import { usePaymentsStore } from '@/stores/payments';
+import { useRouter } from 'vue-router';
+import { useCartStore } from '@/stores/cart';
+
+const cartStore = useCartStore();
+const paymentStore = usePaymentsStore();
+const router = useRouter()
+const computedTitle = computed(() => {
+  if (paymentStore.paymentAuthority) {
+    return 'پرداخت'
+  }
+  return 'ثبت سفارش'
 })
 
+definePageMeta({
+  layout: false,
+  title: computedTitle.value
+})
+
+//watch if cart is empty redirect to cart page
+watch(cartStore.cartItems, (newVal) => {
+  if (newVal.length === 0) {
+    router.push('/checkout/cart')
+  }
+})
+
+onMounted(async () => {
+  await cartStore.fetchCart();
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
