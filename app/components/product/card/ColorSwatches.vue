97ad5087431ad758d1e4a16ccebab51e91ea5bc5
erikheydari <erikheydari@gmail.com>
2025-08-11 14:24:12 +0330

    Refactor ColorSwatches component to integrate Tooltip functionality, enhancing user experience by displaying color names on hover. Update props to include class for better styling control.

--- app/components/product/card/ColorSwatches.vue
+++ app/components/product/card/ColorSwatches.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="flex -space-y-2" :class="props.orientation === 'horizontal' ? 'flex-row' : 'flex-col'" dir="ltr">
-    <span v-for="(color, index) in props.colors" :key="index" :style="{ backgroundColor: color.value }"
-      class="rounded-full border-2 border-background hover:scale-105 transition-transform duration-300"
-      :class="sizeClass">
-    </span>
-  </div>
+  <TooltipProvider dir="ltr">
+    <div class="flex -space-y-2 w-fit!" :class="props.orientation === 'horizontal' ? 'flex-row' : 'flex-col' + ' ' + props.class" >
+      <Tooltip v-for="(color, index) in props.colors" :key="index">
+        <TooltipTrigger as-child>
+          <span :style="{ backgroundColor: color.value }"
+            class="rounded-full border-2 border-background hover:scale-105 transition-transform duration-300"
+            :class="sizeClass">
+          </span>
+        </TooltipTrigger>
+        <TooltipContent class="z-55">
+          <p :class="props.size === 'sm' ? 'caption-1' : 'body-2'">{{ color.name }}</p>
+        </TooltipContent>
+      </Tooltip>
+    </div>
+  </TooltipProvider>
 </template>
 
 <script lang="ts" setup>
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '~/components/ui/tooltip';
+
 const props = withDefaults(defineProps<{
   size?: 'sm' | 'md' | 'lg'
   orientation?: 'horizontal' | 'vertical'
+  class?: string
   colors: {
     value: string
     name: string
