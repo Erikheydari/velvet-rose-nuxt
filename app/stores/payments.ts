c0ee4a145cf03fe303c8a6339001936cfc1c0166
erikheydari <erikheydari@gmail.com>
2025-08-11 18:39:33 +0330

    Add payments store for state management and payment initiation, including methods for setting and clearing payment URLs and authorities.

---
+++ app/stores/payments.ts
@@ -0,0 +1,60 @@
+import { defineStore } from 'pinia'
+import { useApiStore } from './api'
+import { useEndpointStore } from './endpoints'
+
+export const usePaymentsStore = defineStore('paymentsStore', () => {
+  const apiStore = useApiStore()
+  const endpoints = useEndpointStore()
+
+  const loading = ref(false)
+  const error = ref<string | null>(null)
+
+  const paymentAuthority = ref<string | null>(null)
+  const paymentUrl = ref<string | null>(null)
+
+  const initiatePayment = async () => {
+    if (!paymentUrl.value) return
+    window.location.href = paymentUrl.value
+    clearPayment()
+    /*     const { data, error: apiError } = await apiStore.apiRequest<undefined, any>(endpoints.payments.initiate, {
+          method: 'post',
+          credentials: true,
+        })
+        if (apiError) {
+          error.value = apiError
+        }
+        if (data) {
+          setPaymentUrl(data.payment_url, data.authority)
+        } */
+  }
+
+  const setPaymentUrl = (url: string, authority: string) => {
+    paymentUrl.value = url
+    paymentAuthority.value = authority
+    if (import.meta.client) {
+      sessionStorage.setItem('paymentUrl', url)
+      sessionStorage.setItem('authority', authority)
+    }
+  }
+
+  const clearPayment = () => {
+    paymentUrl.value = null
+    paymentAuthority.value = null
+    if (import.meta.client) {
+      sessionStorage.removeItem('paymentUrl')
+      sessionStorage.removeItem('authority')
+    }
+  }
+
+  return {
+    // state
+    loading,
+    error,
+    paymentAuthority,
+    paymentUrl,
+
+    // actions
+    initiatePayment,
+    setPaymentUrl,
+  }
+}) 
