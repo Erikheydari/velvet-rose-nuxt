4dcf5eaec01d8d7f4c32f958988814d6d231a71c
erikheydari <erikheydari@gmail.com>
2025-08-11 19:05:58 +0330

    Add Empty cart component to display a message when the cart is empty, including a button to navigate to the products page.

--- app/components/cart/Popover.vue
+++ app/components/cart/Popover.vue
@@ -40,18 +40,9 @@
         </TheButton>
       </div>
 
-      <div v-else class="flex flex-col gap-4 py-10">
-        <div class="relative w-fit mx-auto">
-          <ShoppingCart class="size-10 mx-auto overflow-visible text-muted-foreground/40" />
-          <X class="size-4 mx-auto overflow-visible absolute bottom-1/2 left-10/12 text-destructive z-10" />
-        </div>
-        <p class="body-2 text-muted-foreground mx-auto">
-          سبد خرید شما خالی است
-        </p>
-        <TheButton v-if="route.path !== '/products'" variant="tonal" size="sm" class="w-fit mx-auto" to="/products">
-          فروشگاه
-        </TheButton>
-      </div>
+      <template v-else>
+        <CartEmpty class="h-full flex flex-col justify-center items-center gap-4" />
+      </template>
     </div>
   </Transition>
   <Backdrop :is-active="isActive" :handle-close="handleClose" />
