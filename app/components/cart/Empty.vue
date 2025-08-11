d810bdd6850f0f74a00b4f1dc900b44e5b41b9ef
erikheydari <erikheydari@gmail.com>
2025-08-11 19:05:54 +0330

    Add Empty component for cart to display a message when the cart is empty, including a button to navigate to the products page.

---
+++ app/components/cart/Empty.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="class">
+    <div class="relative w-fit mx-auto">
+      <ShoppingCart class="size-10 mx-auto overflow-visible text-muted-foreground/40" />
+      <X class="size-4 mx-auto overflow-visible absolute bottom-1/2 left-10/12 text-destructive z-10" />
+    </div>
+    <p class="body-2 text-muted-foreground mx-auto">
+      سبد خرید شما خالی است
+    </p>
+    <TheButton v-if="route.path !== '/products'" variant="tonal" size="sm" class="w-fit mx-auto" to="/products">
+      فروشگاه
+    </TheButton>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+import { ShoppingCart, X } from 'lucide-vue-next';
+
+const route = useRoute();
+
+const props = defineProps<{
+  class?: string;
+}>();
+</script>
+
+<style></style>
