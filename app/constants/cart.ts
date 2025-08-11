0011e6cf2c602a33ce92a957a058ac2876f13886
erikheydari <erikheydari@gmail.com>
2025-08-11 16:38:48 +0330

    Add cart constant with sample product data including details like price, discount, and selected attributes for cart management.

---
+++ app/constants/cart.ts
@@ -0,0 +1,74 @@
+export const cart = {
+    "data": [
+      {
+        "id": 53,
+        "product": {
+          "id": 4,
+          "name": "لیپ گلاس Kiko",
+          "full_name": "Lip Gloss Kiko",
+          "price": "300,000",
+          "discount_percentage": "2",
+          "final_price": "294,000",
+          "category": {
+            "name": "آرایشی بهداشتی",
+            "slug": "hygiene-cosmetics"
+          },
+          "brand": {
+            "name": "کیکو",
+            "slug": null
+          },
+          "image": {
+            "src": "http://78.157.42.110/storage/products/JPTf6rqop5DZ0sj9ZIFTRVDXOZDoOqK3KagrzpCA.png",
+            "alt_image": "کیکو"
+          },
+          "slug": "kiko-lip-gloss",
+          "selected_attributes": [
+            {
+              "id": 147,
+              "attribute": "color",
+              "value": "گلبهی",
+              "name": "#C00B0B"
+            }
+          ]
+        },
+        "quantity": 1,
+        "line_total_without_discount": "300,000",
+        "line_total_with_discount": "294,000"
+      },
+      {
+        "id": 55,
+        "product": {
+          "id": 4,
+          "name": "لیپ گلاس Kiko",
+          "full_name": "Lip Gloss Kiko",
+          "price": "300,000",
+          "discount_percentage": "2",
+          "final_price": "294,000",
+          "category": {
+            "name": "آرایشی بهداشتی",
+            "slug": "hygiene-cosmetics"
+          },
+          "brand": {
+            "name": "کیکو",
+            "slug": null
+          },
+          "image": {
+            "src": "http://78.157.42.110/storage/products/JPTf6rqop5DZ0sj9ZIFTRVDXOZDoOqK3KagrzpCA.png",
+            "alt_image": "کیکو"
+          },
+          "slug": "kiko-lip-gloss",
+          "selected_attributes": [
+            {
+              "id": 146,
+              "attribute": "color",
+              "value": "سبز",
+              "name": "#000000"
+            }
+          ]
+        },
+        "quantity": 2,
+        "line_total_without_discount": "600,000",
+        "line_total_with_discount": "588,000"
+      }
+    ]
+  }
