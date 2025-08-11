5b4b07063b70514033bbc63fed2a7672dfd343c9
erikheydari <erikheydari@gmail.com>
2025-08-11 14:58:20 +0330

    Fix formatting in Products.vue by adding a space after the loading prop in ProductCarousel for improved code readability.

--- app/components/sections/home/Products.vue
+++ app/components/sections/home/Products.vue
@@ -2,7 +2,7 @@
   <section class="bg-secondary py-10">
     <slot />
 
-    <ProductCarousel :products="products" :type="props.type" :align-slider="alignSlider" class="mb-8" :loading="props.loading" />
+    <ProductCarousel :products="products" :type="props.type" :align-slider="alignSlider" class="mb-8" :loading="props.loading"   />
 
 
     <div class="flex justify-center">
