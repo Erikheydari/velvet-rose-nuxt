d5cbb70418ff42e56c037e5f0e6b78395dfb6047
erikheydari <erikheydari@gmail.com>
2025-08-11 14:58:35 +0330

    Add color swatches slot to ProductCard in product grid for enhanced customization of product attributes.

--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -3,7 +3,13 @@
     <SectionsProductsHero />
     <SectionsProductsBanners class="mb-8 lg:mb-12" />
     <ProductGrid class="mb-8 lg:mb-12">
-      <ProductCard v-for="product in productsStore.products" :key="product.id" :product="product" type="default" />
+      <ProductCard v-for="product in productsStore.products" :key="product.id" :product="product" type="default">
+        <template #swatches>
+          <ProductCardColorSwatches v-if="product.attributes.color && product.attributes.color.length > 0"
+            :colors="product.attributes.color" class="absolute! top-4! right-4! z-2" orientation="vertical"
+            size="md" />
+        </template>
+      </ProductCard>
     </ProductGrid>
   </div>
 </template>
