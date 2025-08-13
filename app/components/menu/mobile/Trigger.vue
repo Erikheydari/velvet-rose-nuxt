c2451ab2855009692f39cf0009042975ae969587
erikheydari <erikheydari@gmail.com>
2025-08-13 20:27:25 +0330

    Enhance mobile Trigger component by adding props for custom classes and updating icon size for improved styling and flexibility.

--- app/components/menu/mobile/Trigger.vue
+++ app/components/menu/mobile/Trigger.vue
@@ -1,11 +1,17 @@
 <template>
-  <TheButton variant="tonal" size="icon-lg" class="shrink-0 md:hidden" @click="toggleMenu">
-    <Menu class="w-4 h-4" />
+  <TheButton variant="tonal" size="icon-lg" class="shrink-0 lg:hidden" :class="props.class"
+    @click="toggleMenu">
+    <Menu class="size-5" :class="props.iconClass" />
   </TheButton>
 </template>
 
 <script lang="ts" setup>
 import { Menu } from 'lucide-vue-next'
+
+const props = defineProps<{
+  class?: string
+  iconClass?: string
+}>()
 
 const emit = defineEmits(['toggle'])
 
