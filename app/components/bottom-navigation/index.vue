fd070cfcb2290890e6372c5c51a74c15797b0e3b
erikheydari <erikheydari@gmail.com>
2025-08-13 19:37:02 +0330

    Add BottomNavigation component to enhance user interface with a fixed navigation bar for mobile devices.

---
+++ app/components/bottom-navigation/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Teleport to="body">
+    <div class="fixed bottom-0 left-0 w-full z-50 p-4 lg:hidden">
+      <div class="background-backdrop bg-background/90! gap-4 p-4 rounded-lg"
+        :class="props.class">
+        
+        <slot />
+
+      </div>
+    </div>
+
+  </Teleport>
+</template>
+
+<script lang="ts" setup>
+
+const props = defineProps<{
+  class?: string
+}>()
+
+</script>
+
+<style></style>
