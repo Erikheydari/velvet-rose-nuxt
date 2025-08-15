85e326a2c884d8a3786b9c31d35df1a3fc3f2af1
erik <erikheydari@gmail.com>
2025-08-15 20:04:19 +0330

    Refactor Category component to enhance layout and improve image handling. Update to use NuxtLink for category navigation and adjust image classes for better responsiveness and loading effects.

--- app/components/sections/home/Category.vue
+++ app/components/sections/home/Category.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="h-screen flex flex-col justify-center items-center">
+  <div class="h-screen flex flex-col justify-center items-center overflow-hidden">
     <slot />
-    <figure class="w-[50vw] h-[70%] bg-image bg-background" :class="{ 'bg-image-loaded': isImageLoaded }">
+    <NuxtLink :to="`/products/${props.category?.slug}`" class="w-[130vw] max-h-[50vh] lg:w-[50vw] lg:h-[70%] bg-image bg-background" :class="{ 'bg-image-loaded': isImageLoaded }">
       <img :src="props.category?.banner" alt="category banner"
         :class="{ 'opacity-100': isImageLoaded, 'opacity-0': !isImageLoaded }"
-        class="w-full h-full max-w-[35vw] mx-auto object-contain mix-blend-multiply transition-opacity duration-300"
+        class="w-full h-full max-w-[100vw] lg:max-w-[35vw] mx-auto object-cover lg:object-contain mix-blend-multiply transition-opacity duration-300"
         @load="handleImageLoad" />
-    </figure>
+    </NuxtLink>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { NuxtLink } from '#components';
 import type { Category } from '~/types/product.types';
 
 const props = defineProps<{
