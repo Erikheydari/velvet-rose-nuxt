32e99a0786c319cd173cf49c629f762e67bc8900
erik <erikheydari@gmail.com>
2025-08-15 17:11:41 +0330

    Add OTP verification page for Forgot Password functionality, including template, script, and meta information for navigation and layout management.

---
+++ app/pages/auth/forgot-password/otp.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col gap-4 w-full">
+    <h1 class="heading-5 font-bold text-primary text-center w-full">
+      تایید کد
+    </h1>
+    <FormForgotPasswordOtp />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import FormForgotPasswordOtp from '@/components/form/ForgotPasswordOtp.vue'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+
+if (!authStore.isForgotPasswordOtpSent) {
+  navigateTo('/auth/forgot-password')
+}
+
+definePageMeta({
+  layout: 'auth',
+  middleware: 'guest'
+})
+
+useHead({
+  title: 'تایید کد',
+  meta: [
+    { name: 'description', content: 'تایید کد' }
+  ]
+})
+</script>
+
+<style></style>
