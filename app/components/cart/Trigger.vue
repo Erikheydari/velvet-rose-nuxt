89499bd1d0bc6a2432d00123086f778044d5bdad
erikheydari <erikheydari@gmail.com>
2025-08-16 20:31:16 +0330

    Refactor cart Trigger component to support dynamic button sizes. Updated props to include a default size option and adjusted template to reflect size changes in button and icon rendering, enhancing flexibility and visual consistency.

--- app/components/cart/Trigger.vue
+++ app/components/cart/Trigger.vue
@@ -3,9 +3,12 @@ import { ShoppingCart } from 'lucide-vue-next'
 import { useCartStore } from '~/stores/cart'
 import { ref, computed, watch } from 'vue'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   handleButtonClick: () => void
-}>()
+  size?: 'icon' | 'icon-lg'
+}>(), {
+  size: 'icon-lg'
+})
 
 const cartStore = useCartStore()
 const cartCount = computed(() => cartStore.cartItemsCount)
@@ -47,20 +50,13 @@ const handleTriggerClick = (event: MouseEvent | KeyboardEvent) => {
 </script>
 
 <template>
-  <TheButton
-    variant="tonal"
-    size="icon-lg"
-    class="relative"
-    :class="{ buzzing: isBuzzing }"
-    @click="handleTriggerClick"
-    aria-label="سبد خرید"
-  >
-    <ShoppingCart class="size-5" />
+  <TheButton variant="tonal" :size="props.size" class="relative" :class="{ buzzing: isBuzzing }"
+    @click="handleTriggerClick" aria-label="سبد خرید">
+    <ShoppingCart :class="props.size === 'icon' ? 'size-4' : 'size-5'" />
 
-    <span
-      v-if="cartCount > 0"
-      class="absolute -top-1 -right-1 min-w-5 h-5 px-1 rounded-full bg-primary text-primary-foreground text-xs leading-none flex items-center justify-center shadow-sm"
-    >
+    <span v-if="cartCount > 0"
+      :class="props.size === 'icon' ? 'size-4' : 'size-5'"
+      class="absolute -top-1 -right-1  px-1 rounded-full bg-primary text-primary-foreground text-xs leading-none flex items-center justify-center shadow-sm">
       {{ cartCount }}
     </span>
   </TheButton>
