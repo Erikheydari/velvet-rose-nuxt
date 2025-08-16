8b85993c592d6521392ffafaa785377303a1db21
erikheydari <erikheydari@gmail.com>
2025-08-16 20:31:06 +0330

    Update bottom navigation styles for improved layout and spacing. Adjusted padding in the template to enhance visual consistency across devices.

--- app/components/bottom-navigation/index.vue
+++ app/components/bottom-navigation/index.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport to="body">
-    <div class="fixed bottom-0 left-0 w-full z-50 p-4 lg:hidden">
-      <div class="background-backdrop bg-background/90! gap-4 p-4 rounded-lg"
+    <div class="fixed bottom-0 left-0 w-full z-50 p-2 lg:hidden">
+      <div class="background-backdrop bg-background/90! gap-4 p-2 rounded-lg"
         :class="props.class">
         
         <slot />
