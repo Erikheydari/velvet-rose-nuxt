2f900fa6e7d4158b3401afa86c4fbbee2d70f79e
erikheydari <erikheydari@gmail.com>
2025-08-16 15:14:01 +0330

    Refactor FantastyHeading component styles and structure. Updated heading element to use uppercase text with a gradient background, enhancing visual appeal. Removed unnecessary background gradient div and streamlined the template for better readability.

--- app/components/ui/heading/FantastyHeading.vue
+++ app/components/ui/heading/FantastyHeading.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex flex-col justify-center items-center relative lg:mb-20">
-    <h3 class="text-primary relative z-0 leading-none" :class="props.titleClass">
+    <h3 
+      class="relative z-0 leading-none uppercase" 
+      :class="props.titleClass"
+    >
       {{ props.title }}
     </h3>
-    <div class="background-gradient absolute left-0 bottom-0 w-full z-1"></div>
     <p class="relative z-2 leading-none -mt-[2%] text-primary" :class="props.descriptionClass">
       {{ props.description }}
     </p>
@@ -11,7 +13,6 @@
 </template>
 
 <script lang="ts" setup>
-//with default values
 const props = withDefaults(defineProps<{
   title: string;
   description: string;
@@ -24,9 +25,10 @@ const props = withDefaults(defineProps<{
 </script>
 
 <style scoped>
-.background-gradient {
-  height: clamp(5rem, 14vh, 12rem);
-  background: var(--color-background);
-  background: linear-gradient(0deg, rgba(255, 255, 255, 1) 20%, rgba(255, 255, 255, 0.9) 40%, rgba(255, 255, 255, 0) 100%);
+h3 {
+  background: linear-gradient(180deg, var(--color-primary) 0%, rgba(255, 255, 255, 0) 60%, rgba(255, 255, 255, 0) 140%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
 }
 </style>
